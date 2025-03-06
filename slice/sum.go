1c1385f9be3f6fc2bcb56b967ee818fccac705b3
xfelix <xavierfelix74@gmail.com>
2025-03-05 16:34:09 -0800

    updated

--- slice/sum.go
+++ slice/sum.go
@@ -7,3 +7,36 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// func SumAll(arraysOfNumbers ...[]int) []int {
+
+// 	numberOfArrays := len(arraysOfNumbers)
+// 	sums := make([]int, numberOfArrays)
+
+// 	for i, currentArray := range arraysOfNumbers {
+// 		sums[i] = Sum(currentArray)
+// 	}
+
+// 	return sums
+// }
+
+func SumAll(arraysOfNumbers ...[]int) []int {
+	var sums []int
+	for _, current := range arraysOfNumbers {
+		sums = append(sums, Sum(current))
+	}
+	return sums
+}
+
+func SumAllTails(arraysOfnumbers ...[]int) []int {
+	var sum []int
+	for _, current := range arraysOfnumbers {
+		if len(current) == 0 {
+			sum = append(sum, 0)
+		} else {
+			tail := current[1:]
+			sum = append(sum, Sum(tail))
+		}
+	}
+	return sum
+}
