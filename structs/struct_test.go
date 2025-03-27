5377ade105beeeb2edb1bc0e3366835a25af2810
XaviFelix <xavierfelix74@gmail.com>
2025-03-26 22:44:04 -0700

    updated tdd

--- structs/struct_test.go
+++ structs/struct_test.go
@@ -40,13 +40,14 @@ func TestArea(t *testing.T) {
 
 func TestArea2(t *testing.T) {
 
-	// anonymous struct
+	// anonymous struct (table driven tests)
 	areaTests := []struct {
 		shape Shape
 		want  float64
 	}{
 		{Rectangle{12, 6}, 72.0},
 		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 
 	for _, currentShape := range areaTests {
